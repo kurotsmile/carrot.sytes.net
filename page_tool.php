cba1e2bf59da0e5ce90c52757f1dd3cf401bc071
Trần Thiện Thanh <kurotsmile@gmail.com>
2020-03-14 10:59:14 +0700

    thêm công cụ sao dữ liệu
    Signed-off-by: Trần Thiện Thanh <kurotsmile@gmail.com>

--- page_tool.php
+++ page_tool.php
@@ -16,3 +16,9 @@
     <strong>Kiểm tra các tệp tin âm nhạc thiếu thông số</strong>
     <span>Kiểm tra các đối tượng tệp tin thiếu thông số âm nhạc</span>
 </a>
+
+<a  href="<?php echo $url;?>?view=page_tool_backup_database" class="tool_item">
+    <i class="fa fa-database" aria-hidden="true"></i>
+    <strong>Sao lưu dữ liệu</strong>
+    <span>Sao lưu lại cơ sở dư liệu không bao gồm các các tệp tin vật lý</span>
+</a>
